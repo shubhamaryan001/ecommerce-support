d719e8caababff0ab45a65b1e5a601cdd93a211f
Shubham <saurabharyan30@gmail.com>
2019-10-31 17:52:48 +0530

    update for deployment-2

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/Chat-support'));
+app.use(express.static(__dirname + '/ecommerce-support'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/Chat-support/index.html'));
+res.sendFile(path.join(__dirname+'/ecommerce-support/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
