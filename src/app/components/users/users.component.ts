d333cdde3e91f261b999edf45a90546e02650d92
Shubham <saurabharyan30@gmail.com>
2019-10-31 17:10:43 +0530

    push support version 1.1

---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { TokenService } from 'src/app/services/token.service';
+import { Router } from '@angular/router';
+
+const API = 'http://localhost:8000/api';
+
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+  user: any;
+photoUrl:any;
+  constructor(private tokenService: TokenService, private router: Router) { }
+
+  ngOnInit() {
+
+    this.user = this.tokenService.GetUserInfo().user;
+    console.log(this.tokenService.GetUserInfo());
+
+    this.photoUrl = this.user._id
+          ? `${API}/user/photo/${
+              this.user._id
+            }?${new Date().getTime()}`
+          : "";
+
+  }
+
+
+  ChatPage() {
+      this.router.navigate(['people']);
+  }
+
+}
